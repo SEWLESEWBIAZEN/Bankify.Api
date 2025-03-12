e659f5adc2779915944509299959dfc27bfdcf3d
sewlesewb <sewlesewb@PCHQWPR13>
2025-03-12 11:24:18 +0300

    refactor: refactoring the db context structure

--- Bankify.Infrastructure/Context/BankifyDbContext.cs
+++ Bankify.Infrastructure/Context/BankifyDbContext.cs
@@ -14,75 +14,88 @@ namespace Bankify.Infrastructure.Context
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Account>()
-                .HasOne(p => p.User)
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+                entity.HasOne(p => p.User)
                 .WithMany(cc => cc.Accounts)
                 .HasForeignKey(ph => ph.UserId)
                 .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<Account>()                
-               .HasOne(p => p.AccountType)
+                entity.HasOne(p => p.AccountType)
                .WithMany(cc => cc.Accounts)
-               .HasForeignKey(ph => ph.AccountTypeId)               
+               .HasForeignKey(ph => ph.AccountTypeId)
                .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<Account>()
-                .Property(p => p.Balance)
+
+                entity.Property(p => p.Balance)
                 .HasPrecision(38, 10);
 
+            });
+            modelBuilder.Entity<Transfer>(entity => {
+            entity.HasKey(x => x.Id);
+
+            entity.HasOne(t => t.TransferedFrom)
+            .WithMany(a => a.TransfersFrom)
+            .HasForeignKey(fh => fh.TransferedFromId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+             entity.HasOne(t => t.TransferedTo)
+            .WithMany(a => a.TransfersTo)
+            .HasForeignKey(fh => fh.TransferredToId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+            entity.Property(t => t.AmmountTransfered)
+            .HasPrecision(38, 10);
+
+            });            
+                                      
             modelBuilder.Entity<AccountType>()
                 .Property(tt => tt.InterestRate)
                 .HasPrecision(5, 2);
 
-            modelBuilder.Entity<Transfer>()
-                .HasOne(t => t.TransferedFrom)
-                .WithMany(a => a.TransfersFrom)
-                .HasForeignKey(fh => fh.TransferedFromId)
-                .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<Transfer>()
-                .HasOne(t=>t.TransferedTo)
-                .WithMany(a=>a.TransfersTo)
-                .HasForeignKey(fh=>fh.TransferredToId) 
-                .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<Transfer>()
-                .Property(t=>t.AmmountTransfered)
-                .HasPrecision(38, 10);
-
             //transaction entries
-            modelBuilder.Entity<TransactionEntry>()
-                .HasOne(t => t.Transaction)
-                .WithMany(tr=>tr.TransactionEntries)
+            modelBuilder.Entity<TransactionEntry>(entity => { 
+                entity.HasKey(t => t.Id);
+                entity.HasOne(t => t.Transaction)
+                .WithMany(tr => tr.TransactionEntries)
                 .HasForeignKey(fh => fh.TransactionId)
                 .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<TransactionEntry>()
-                .HasOne(t => t.Account)
-                .WithMany(tr=>tr.TransactionEntries)
+                entity.HasOne(t => t.Account)
+                .WithMany(tr => tr.TransactionEntries)
                 .HasForeignKey(fh => fh.AccountId)
                 .OnDelete(DeleteBehavior.Restrict);
-            
-            //Authorization           
+                entity.Property(e => e.BalanceBeforeTransaction).HasPrecision(25, 5);
+                entity.Property(e=>e.Amount).HasPrecision(25, 5);
+                entity.Property(e=>e.BalanceAfterTransaction).HasPrecision(25, 5);
 
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur => ur.AppRole)
+            });
+
+            //Authorization          
+
+            modelBuilder.Entity<UserRole>(entity => { 
+                entity.HasKey(t => t.Id);
+                entity.HasOne(ur => ur.AppRole)
                 .WithMany(ar => ar.UserRoles)
                 .HasForeignKey(fh => fh.AppRoleId)
                 .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<UserRole>()
-                .HasOne(ur=>ur.AppUser)
-                .WithMany(au=>au.UserRoles)
-                .HasForeignKey(fh=>fh.AppUserId)
+                entity.HasOne(ur => ur.AppUser)
+                .WithMany(au => au.UserRoles)
+                .HasForeignKey(fh => fh.AppUserId)
                 .OnDelete(DeleteBehavior.Restrict);
-            modelBuilder.Entity<RoleClaim>()
-                .HasOne(rc => rc.AppClaim)
+            });
+
+
+            modelBuilder.Entity<RoleClaim>(entity => { 
+                entity.HasKey(e => e.Id);
+                entity.HasOne(rc => rc.AppClaim)
                 .WithMany(ac => ac.RoleClaims)
                 .HasForeignKey(fh => fh.AppClaimId)
                 .OnDelete(DeleteBehavior.Restrict);
-
-            modelBuilder.Entity<RoleClaim>()
-                .HasOne(rc => rc.AppRole)
+                entity.HasOne(rc => rc.AppRole)
                 .WithMany(ar => ar.RoleClaims)
                 .HasForeignKey(fh => fh.AppRoleId)
                 .OnDelete(DeleteBehavior.Restrict);
-
+            });                            
         }
 
         #region Users
